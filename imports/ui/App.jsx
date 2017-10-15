93d73c840af59ea22d691f9c70028a840186c4b1
toaderflorin <toaderflorin@gmail.com>
2017-10-15 21:15:20 +0300

    Added filtering to accounts.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.logoutClick = this.logoutClick.bind(this)  
+    this.logoutClick = this.logoutClick.bind(this)
   }
 
   handleSubmit(e) {
@@ -23,12 +23,9 @@ class App extends Component {
     FlowRouter.go('/login')
   }
 
-
   logoutClick(e) {
     Meteor.logout(function(error) {
-      if (error) {
-        console.log('Logout failed.', error)
-      } else {
+      if (!error) {
         window.location = '/'
       }
     })
@@ -41,26 +38,31 @@ class App extends Component {
     })
 
     return (
-      <div className="container">
-        <h1>The Store</h1>
-        <div>
-          {!Meteor.user() && <a href="/login" onClick={this.loginClick}>Log in</a>}
-          {Meteor.user() && <a href="/" onClick={this.logoutClick}>Log out</a>}
+      <div className="root">
+        <div className="header">
+          <div className="container">
+            <h1>The Store</h1>
+            <div>
+              {!Meteor.user() && <a href="/login" onClick={this.loginClick}>Log in</a>}
+              {Meteor.user() && <div>Welcome <b><i>{Meteor.user().emails[0].address}</i></b>, <a href="/" onClick={this.logoutClick}>log out</a></div>}
+
+              <br/>
+
+              {Meteor.userId() ?
+                <div>
+                  <input className="add-product" type="text" ref="textInput" placeholder="Type to add new products" />
+                  <button className="add-button" onClick={this.handleSubmit}>Add</button>
+                  <br/>
+                </div> : ''
+              }
+            </div>
+          </div>
         </div>
-
-        <br/>
-
-        {Meteor.userId() ?
-          <div>
-            <form onSubmit={this.handleSubmit} >
-              <input className="add-product"  type="text" ref="textInput" placeholder="Type to add new products" />
-            </form>
-            <br/>
-          </div> : ''
-        }
-
-        <div className="product-list">
-          {renderedProducts}
+        <div className="container">
+          <br/>
+          <div className="product-list">
+            {renderedProducts}
+          </div>
         </div>
       </div>
     )
