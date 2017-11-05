cd878212148e0bed15c4ff15faf8448fe421c221
toaderflorin <toaderflorin@gmail.com>
2017-11-05 10:51:28 +0200

    Added header for redirecting.

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
   constructor(props) {
     super(props)
+    console.log(Link)
     this.logoutClick = this.logoutClick.bind(this)
   }
 
@@ -24,13 +26,16 @@ export default class Header extends Component {
         <div className="container">
           <h1>The Store</h1>
           <div>
+            <Link to='/'>Home</Link> | <Link to='/admin'>Admin</Link>
+          </div>
+          <p>
             {!Meteor.user() ? <a href="/login" onClick={this.loginClick}>Log in</a> : ''}
             {Meteor.user() ?
               <div>Welcome <b><i>{Meteor.userId()}</i></b>
               <br/>
               <a href="/" onClick={this.logoutClick}>log out</a></div>
               : ''}
-          </div>
+          </p>
         </div>
       </div>
     )
