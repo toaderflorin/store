78f93f209dcb5499b44cd8c8085784ff6eb7d329
toaderflorin <toaderflorin@gmail.com>
2017-11-18 18:30:34 +0200

    Added image to basket item.

--- imports/ui/BasketItem.jsx
+++ imports/ui/BasketItem.jsx
@@ -9,6 +9,8 @@ export default class BasketItem extends Component {
   render() {
     return (
       <div className="basket-item">
+        <div className="basket-item-image"
+          style={{ backgroundImage: `url('${this.props.product.product.url}')` }}></div>
         {this.props.product.product.text}({this.props.product.count})
         <button className="buy" onClick={this.props.deleteClick}>Delete</button>
       </div>
